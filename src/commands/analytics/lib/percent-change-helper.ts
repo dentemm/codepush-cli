893ee468f19b4e8237c797d85911cc798470f95f
dentemm <tim.claes@me.com>
2024-10-17 18:22:22 +0200

    chore: clean unneeded code

--- src/commands/analytics/lib/percent-change-helper.ts
+++
@@ -1,9 +0,0 @@
-export function calculatePercentChange(currentValue: number, previousValue: number) {
-  if (previousValue !== 0) {
-    return ((currentValue - previousValue) / previousValue) * 100;
-  } else if (currentValue === 0) {
-    return 0;
-  } else {
-    return 100;
-  }
-}
--- src/commands/test/lib/test-cloud-error.ts
+++
@@ -1,9 +0,0 @@
-export class TestCloudError extends Error {
-  public exitCode?: number;
-
-  constructor(message: string, errorCode?: number) {
-    super(message);
-
-    this.exitCode = errorCode;
-  }
-}
